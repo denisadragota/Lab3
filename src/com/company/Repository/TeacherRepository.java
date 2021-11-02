cc216fbcf72e2bfffd46cbff36eb39080564c5f4
Denisa Dragota <81757841+denisadragota@users.noreply.github.com>
2021-11-02 17:49:42 +0200

    Update TeacherRepository.java

--- src/com/company/Repository/TeacherRepository.java
+++ src/com/company/Repository/TeacherRepository.java
@@ -21,17 +21,17 @@ public class TeacherRepository extends InMemoryRepository<Teacher>{
         }
 
     /**
+     * desc: finds a teacher object by id
      * @param id -the id of the entity to be returned id must not be null
      * @return the entity with the specified id or null - if there is no entity with the given id
      * @throws NullException if input parameter id is NULL
      */
         @Override
         public Teacher findOne(Long id) throws NullException {
-            /* Exception */
             if(id==null)
                 throw new NullException("Null id!");
 
-            /* loop through all teachers in the repo and find id */
+           
             for(Teacher t: this.repoList)
             {
                 if(t.getTeacherId()==id)
@@ -41,37 +41,36 @@ public class TeacherRepository extends InMemoryRepository<Teacher>{
         }
 
     /**
+     * desc: adding a Teacher object to the repo list
+     * first checking if already exist, then adding
      * @param obj entity must be not null
      * @return null- if the given entity is saved otherwise returns the entity (id already exists)
      * @throws NullException if input parameter entity obj is NULL
-     * adding a Teacher object to the repo list
-     * first checking if already exist, then adding
      */
         @Override
         public Teacher save(Teacher obj) throws NullException {
-            /* Exception */
+       
             if(obj==null)
                 throw new NullException("Null obj!");
 
-            /* if object already exists in the repo */
             if (this.findOne(obj.getTeacherId()) != null)
                 return obj;
 
-            /* add object */
+     
             this.repoList.add(obj);
             return null;
         }
 
     /**
+     * desc: finds old instance with the same id as the new updated given object
+     * removes the old instance and adds the updated one
      * @param obj entity must not be null
      * @return null - if the entity is updated, otherwise returns the entity - (e.g id does not exist).
      * @throws NullException if input parameter entity obj is NULL
-     * finds old instance with the same id as the new updated given object
-     * removes the old instance and adds the updated one
      */
         @Override
         public Teacher update(Teacher obj) throws NullException {
-            /* Exception */
+            
             if(obj==null)
                 throw new NullException("Null object!");
 
@@ -90,15 +89,15 @@ public class TeacherRepository extends InMemoryRepository<Teacher>{
 
 
     /**
+     * desc: deletes object with given id from the repo list
+     * first checks if id exists in the repoList, then delete
      * @param id id must be not null
      * @return the removed entity or null if there is no entity with the given id
      * @throws NullException if input parameter id is NULL
-     * deletes object with given id from the repo list
-     * first checks if id exists in the repoList, then delete
      */
         @Override
         public Teacher delete(Long id) throws NullException {
-            /* Exception */
+        
             if(id==null)
                 throw new NullException("Null id!");
 
